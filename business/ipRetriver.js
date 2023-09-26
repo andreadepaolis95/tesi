9413ddd66e5c378e2f05e512545cd19c074042ea
Andrea De Paolis <andreadepaolis99@gmail.com>
2023-09-26 17:54:46 +0200

    stable v4

--- business/ipRetriver.js
+++ business/ipRetriver.js
@@ -2,41 +2,43 @@ const http = require('http');
 const { getIpCalloutOptions } = require('../config/config')
 
 
-const getCountryFromIp = async(ipAddress) => {
-    // Crea una richiesta HTTP per ottenere lo stato di provenienza
-    let country = 'Italy';
-    const req = http.request(getIpCalloutOptions(ipAddress), (res) => {
-        let data = '';
+const getCountryFromIp = (ipAddress) => {
+    return new Promise((resolve, reject) => {
+        const req = http.request(getIpCalloutOptions(ipAddress), (res) => {
+            let data = '';
 
-        res.on('data', (chunk) => {
-            data += chunk;
-        });
-        
-        res.on('end', () => {
-            try {
-                const result = JSON.parse(data);
-                if (result.status === 'success') {
-                    console.log(`Stato di provenienza per l'indirizzo IP ${ipAddress}: ${result.country}`);
-                    if(result.country != undefined){
-                        country = result.country;
-                        return country;
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            res.on('end', () => {
+                try {
+                    const result = JSON.parse(data);
+                    if (result.status === 'success') {
+                        console.log(`Stato di provenienza per l'indirizzo IP ${ipAddress}: ${result.country}`);
+                        if (result.country !== undefined) {
+                            resolve(result.country);
+                        } else {
+                            reject(new Error('Country is undefined'));
+                        }
+                    } else {
+                        reject(new Error('Impossibile trovare lo stato di provenienza per questo indirizzo IP.'));
                     }
-                } else {
-                    console.error('Impossibile trovare lo stato di provenienza per questo indirizzo IP.');
+                } catch (error) {
+                    reject(new Error('Si è verificato un errore durante l\'analisi della risposta JSON: ' + error));
                 }
-            } catch (error) {
-                console.error('Si è verificato un errore durante l\'analisi della risposta JSON:', error);
-            }
+            });
         });
-    });
 
-    req.on('error', (error) => { 
-        console.error('Si è verificato un errore durante la richiesta HTTP:', error);
-    });
+        req.on('error', (error) => {
+            reject(new Error('Si è verificato un errore durante la richiesta HTTP: ' + error));
+        });
 
-    req.end();
-   
-}
+        req.end();
+    });
+};
+
+
 
 
 
